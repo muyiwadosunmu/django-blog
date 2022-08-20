0bb7925fac98d03654bd4485e39b58777666104e
Muyiwa <oluwamuyiwadosunmu@gmail.com>
2022-08-21 00:05:32 +0100

    Setting up basic models

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Category, Post, Author
 # Register your models here.
+
+admin.site.register(Author)
+admin.site.register(Post)
+admin.site.register(Category)
+
+
+
